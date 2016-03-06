701b1bd001a0a6a85204d815b2450681e83156e5
Nick <lanikolya1989@gmail.com>
2016-03-06 23:45:20 +0200

    Calc with cos&sin

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -83,10 +83,16 @@ public class Main {
             if (action.equals("quit")) {
                 return false;
             }
-            //        } else if (action.equals(6)) {
-//            cos();
-//        } else if (action.equals(7)) {
-//            sin();
+        } else if (action.equals("6")) {
+            res = cos();
+            if (action.equals("quit")) {
+                return false;
+            }
+        } else if (action.equals("7")) {
+            res = sin();
+            if (action.equals("quit")) {
+                return false;
+            }
         } else {
             System.out.println("You entered unknown operation");
             return computing();
@@ -178,32 +184,101 @@ public class Main {
         if (number.equals("quit")) {
             return false;
         }
-        int n1 = Integer.parseInt(number);
+        double n1 = Double.parseDouble(number);
         System.out.println("Enter the power");
         String power = enterTheSecondNumber();
         if (power.equals("quit")) {
             return false;
         }
-        int n2 = Integer.parseInt(power);
-        int res = 1;
-        int[] mas = new int[n2];
-        for (int i = 0; i < mas.length; i++) {
-            mas[i] = n1;
-        }
-        for (int i = 0; i < mas.length; i++) {
-            res = res * mas[i];
+        double n2 = Double.parseDouble(power);
+        double res = n1;
+        for (double i = 1; i < n2; i++) {
+            res = res * n1;
         }
         System.out.print("Answer:" + "\t");
         System.out.println(res);
-        return computing(); 
+        return computing();
     }
 
-//    public static boolean cos(){
-//
-//    }
-//
-//    public static boolean sin(){
-//
-//    }
+    public static boolean cos() {
+        System.out.println("Enter an angle");
+        String angle = enterTheFirstNumber();
+        if (angle.equals("quit")) {
+            return false;
+        }
+//            cos angle = x1 - x2 + x3 - x4 + ... ;
+//        wher x1 = 1 - first member of  series(the angle in radians);
+//             x2 = angle^2/2! - second member of series;
+//             x3 = angle^4/4! - third member of series;
+//             x4 = angle^6/6! - fourth member of series;
+//             etc.
+//        Depending on what accuracy we need, we selecting how much members will have the series.
+//        In our case three members will be enough
+//        So our function will have the form:
+//        cos angle = x1 - x2 + x3;
+        double an = Double.parseDouble(angle);
+        double Pi = 3.1415926535897;
+        double ran = an * Pi / 180;//Transformation degrees in radians
+        System.out.println(ran);
+        double a = powForSinCos(ran, 2);
+        System.out.println("a" + "\t" + a);
+        double b = powForSinCos(ran, 4);
+        double c = factorial(2);
+        double d = factorial(4);
+        double e = powForSinCos(ran, 6);
+        double f = factorial(6);
+        double x1 = 1;
+        double x2 = a / c;
+        double x3 = b / d;
+        double x4 = e / f;
+        double cos = x1 - x2 + x3 - x4;
+        cos = ((double) Math.round(cos * 10000L)) / 10000L;
+        System.out.println("Value of cos" + "\t" + cos);
+        return computing();
+    }
 
+    public static boolean sin() {
+        System.out.println("Enter an angle");
+        String angle = enterTheFirstNumber();
+        if (angle.equals("quit")) {
+            return false;
+        }
+//        sin angle = x1 - x2 + x3 - x4 + x5 + ... ;
+//        wher x1 = angle - first member of  series(the angle in radians);
+//             x2 = angle^3/3! - second member of series;
+//             x3 = angle^5/5! - third member of series;
+//             x4 = angle^7/7! - fourth member of series;
+//             etc.
+//        Depending on what accuracy we need, we selecting how much members will have the series.
+//        In our case three members will be enough
+//        So our function will have the form:
+//        sin angle = x1 - x2 + x3;
+        double an = Double.parseDouble(angle);
+        double Pi = 3.1415926535897;
+        double ran = an * Pi / 180;//Transformation degrees in radians
+        double a = powForSinCos(ran, 3);
+        double b = powForSinCos(ran, 5);
+        double c = factorial(3);
+        double d = factorial(5);
+        double sin = ran - a / c + b / d;
+        sin = ((double) Math.round(sin * 10000L)) / 10000L;
+        System.out.println("Value of sin" + "\t" + sin);
+        return computing();
+    }
+
+    public static double powForSinCos(double base, double power) {
+        double res = base;
+        for (double i = 1; i < power; i++) {
+            res = res * base;
+        }
+        return res;
+    }
+
+    public static double factorial(double number) {
+        double res = 1;
+        for (int i = 2; i <= number; i++) {
+            res = res * i;
+        }
+        return res;
+    }
 }
